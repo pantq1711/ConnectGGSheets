02b51328b779d28497b77920164f6f9f5c3b1826
pantq1711 <hongan.gv10@gmail.com>
2024-03-31 16:46:34 +0700

    src code

--- ReadGoogleSheets.py
+++ ReadGoogleSheets.py
@@ -2,35 +2,45 @@ import gspread
 import pandas as pd
 from oauth2client.service_account import ServiceAccountCredentials
 import mysql.connector
+import schedule
+import time
+from datetime import timedelta, datetime
 
-# Thiết lập thông tin xác thực Google Sheets
-scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive', 'https://www.googleapis.com/auth/spreadsheets']
-credentials = ServiceAccountCredentials.from_json_keyfile_name('C:\Connect\data\secrect_key.json', scopes = scope)
-client = gspread.authorize(credentials)
+def job():
+    # Thiết lập thông tin xác thực Google Sheets
+    scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive', 'https://www.googleapis.com/auth/spreadsheets']
+    credentials = ServiceAccountCredentials.from_json_keyfile_name('C:\\ConnectGGSheets\\data\\secrect_key.json', scopes=scope)
+    client = gspread.authorize(credentials)
 
-# Mở Google Sheet
-sheet = client.open('Your Google Sheet Name').sheet1  
+    # Mở Google Sheet
+    sheet = client.open('MySheets').sheet1  
 
-# Lấy dữ liệu từ Google Sheet
-data = sheet.get_all_values()
+    # Lấy dữ liệu từ Google Sheet
+    data = sheet.get_all_values()
 
-# Tạo DataFrame từ dữ liệu
-df = pd.DataFrame(data[1:], columns=data[0])  
+    # Tạo DataFrame từ dữ liệu
+    df = pd.DataFrame(data[1:], columns=data[0])  
 
-# Kết nối tới MySQL database
-conn = mysql.connector.connect(host='your_host',
-                               user='your_user',
-                               password='your_password',
-                               database='your_database')
-cursor = conn.cursor()
+    # Kết nối tới MySQL database
+    conn = mysql.connector.connect(host='localhost',
+                                   user='root',
+                                   password='',
+                                   database='')
+    cursor = conn.cursor()
 
-# Chuyển dữ liệu từ DataFrame vào MySQL database
-for index, row in df.iterrows():
-    cursor.execute("INSERT INTO your_table (column1, column2, ...) VALUES (%s, %s, ...)", tuple(row))
+    # Chuyển dữ liệu từ DataFrame vào MySQL database
+    for index, row in df.iterrows():
+        cursor.execute("INSERT INTO table (column_name1, column_name2, ...) VALUES (%s, %s, ...)", tuple(row))
 
-# Lưu thay đổi
-conn.commit()
+    # Lưu thay đổi
+    conn.commit()
 
-# Đóng kết nối
-cursor.close()
-conn.close()
+    # Đóng kết nối
+    cursor.close()
+    conn.close()
+
+run = schedule.every(1).hour.do(job)
+
+while True:
+    schedule.run_pending()
+
